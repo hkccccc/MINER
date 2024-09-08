78152c5c2a29664c933768d48ecb8a135a8b5754
Neok <984942152@qq.com>
2024-09-08 08:25:42 +0000

    recover scripts

---
+++ scripts/apply_mask.sh
@@ -0,0 +1,6 @@
+# script for applying mask
+
+python /path/to/main.py --mask text_vqa_5000_0.01 \
+                        --mask_modules vit llm \
+                        --save_output 1
+                        
